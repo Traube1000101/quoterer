a67d0ef89d2688fa65c25c231b338838569806f2
Traube1000101 <girlscoutcookies420@gmx.net>
2024-06-19 21:47:53 +0200

    set custom activity

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const config = require("./config.json");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.on("ready", () => {
+  client.user.setActivity("Searching for quotes...", {
+    type: 4,
+  });
+});
+
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
