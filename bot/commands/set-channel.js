648b022c01e2fc17094966a853f05886a4260f74
Traube1000101 <girlscoutcookies420@gmx.net>
2024-06-26 19:21:01 +0200

    implement saving to db

--- bot/commands/set-channel.js
+++ bot/commands/set-channel.js
@@ -2,33 +2,50 @@ const {
   SlashCommandBuilder,
   ChannelType,
   PermissionFlagsBits,
-  strikethrough,
 } = require("discord.js");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("set-channel")
-    .setDescription("Sets the quote Channel for the current server.")
-    .setDMPermission(false)
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
-    .addChannelOption((option) =>
-      option
-        .setName("channel")
-        .setDescription("Set the Quote Channel")
-        .addChannelTypes(ChannelType.GuildText)
-        .setRequired(true)
-    ),
-  async execute(interaction) {
-    const guildId = interaction.guildId;
-    const channel = interaction.options.getChannel("channel");
-    console.log("Guild ID:", guildId, "\nChannel ID:", channel.id);
-    await interaction.reply({
-      content:
-        "WIP: Nothing was changed or set!" +
-        strikethrough(
-          `Successfully set the quote channel to "${channel.name}".`
-        ),
-      ephemeral: true,
-    });
-  },
+module.exports = (database) => {
+  const serversCollection = database.collection("servers");
+
+  async function setChannel(guildId, channelId, channelName) {
+    try {
+      await serversCollection.updateOne(
+        { _id: guildId },
+        {
+          $set: {
+            channel: {
+              id: channelId,
+              name: channelName,
+            },
+          },
+        },
+        { upsert: true }
+      );
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  return {
+    data: new SlashCommandBuilder()
+      .setName("set-channel")
+      .setDescription("Sets the quote Channel for the current server.")
+      .setDMPermission(false)
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+      .addChannelOption((option) =>
+        option
+          .setName("channel")
+          .setDescription("Set the Quote Channel")
+          .addChannelTypes(ChannelType.GuildText)
+          .setRequired(true)
+      ),
+    async execute(interaction) {
+      const channel = interaction.options.getChannel("channel");
+      setChannel(interaction.guildId, channel.id, channel.name);
+      await interaction.reply({
+        content: `Successfully set the quote channel to "${channel.name}".`,
+        ephemeral: true,
+      });
+    },
+  };
 };
