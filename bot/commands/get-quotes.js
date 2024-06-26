48d99247535427bf0ac6cbc39928e121cc4c4c27
Traube1000101 <girlscoutcookies420@gmx.net>
2024-06-26 22:11:51 +0200

    save quotes to db

--- bot/commands/get-quotes.js
+++ bot/commands/get-quotes.js
@@ -2,62 +2,74 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const { performance } = require("perf_hooks");
 require("dotenv").config({ path: "../../.env" });
 
-function processQuotes(client, messages) {
-  const startTime = performance.now();
-  for (const message of messages) {
-    let quote = {
-      quoteId: message[0],
-      createdTimestamp: message[1].createdTimestamp,
-    };
-    const messageText = message[1].content.replace(/\*\*/g, "");
-
-    const texts = messageText.match(/(?<=")[\w.!?äöü].*?[\w.!?äöü](?=")/g); // Match texts between quotes
-    (texts && (quote.content = texts)) || (quote.invalid = true);
-
-    quote.authors = [];
-
-    const authorIds = messageText.match(/(?<=<@).*?(?=>)/g);
-    if (authorIds) {
-      for (const authorId of authorIds) {
-        const author = client.users.cache.get(authorId);
-        author &&
-          quote.authors.push({
-            id: author.id,
-            name: author.displayName,
-            username: author.username,
-            avatar: author.avatarURL(),
-          });
-      }
-    } else {
-      let byString = messageText.match(/(?<=by\s).*(?=\sin)/); // Match string between by & in
-      if (byString) {
-        byString = byString[0];
-        byString.split(", ").forEach((author) => {
-          quote.authors.push({
-            name: author,
-          });
-        });
-      } else {
-        quote.invalid = true;
-      }
-    }
-    const createdIn = messageText.match(/(?<=\sin\s).*/);
-    (createdIn && (quote.createdIn = createdIn[0])) ||
-      (quote.createdIn = new Date(quote.createdTimestamp).getFullYear());
-
-    quote.invalid && (quote.originalMessage = messageText);
-
-    console.log(`Quote:`, quote);
-  }
-  const endTime = performance.now();
-
-  return {
-    count: messages.size,
-    elapsedTime: (endTime - startTime).toFixed(2),
-  };
-}
-
 module.exports = (database, client) => {
+  function processQuotes(messages) {
+    const startTime = performance.now();
+    for (const message of messages) {
+      let quote = {
+        createdTimestamp: message[1].createdTimestamp,
+      };
+
+      const messageId = message[0];
+      const messageText = message[1].content.replace(/\*\*/g, "");
+      const texts = messageText.match(/(?<=")[\w.!?äöü].*?[\w.!?äöü](?=")/g); // Match texts between quotes
+      (texts && (quote.content = texts)) || (quote.invalid = true);
+
+      quote.authors = [];
+      const authorIds = messageText.match(/(?<=<@).*?(?=>)/g);
+      if (authorIds) {
+        for (const authorId of authorIds) {
+          const author = client.users.cache.get(authorId);
+          author &&
+            quote.authors.push({
+              id: author.id,
+              name: author.displayName,
+              username: author.username,
+              avatar: author.avatarURL(),
+            });
+        }
+      } else {
+        let byString = messageText.match(/(?<=by\s).*(?=\sin)/); // Match string between by & in
+        if (byString) {
+          byString = byString[0];
+          byString.split(", ").forEach((author) => {
+            quote.authors.push({
+              name: author,
+            });
+          });
+        } else {
+          quote.invalid = true;
+        }
+      }
+      const createdIn = messageText.match(/(?<=\sin\s).*/);
+      (createdIn && (quote.createdIn = createdIn[0])) ||
+        (quote.createdIn = new Date(quote.createdTimestamp).getFullYear());
+
+      quote.invalid && (quote.originalMessage = messageText);
+
+      sendNude(messageId, quote);
+    }
+    const endTime = performance.now();
+
+    return {
+      count: messages.size,
+      elapsedTime: (endTime - startTime).toFixed(2),
+    };
+  }
+
+  const quotesCollection = database.collection("quotes");
+  async function sendNude(messageId, quote) {
+    try {
+      await quotesCollection.updateOne(
+        { _id: messageId },
+        { $set: quote },
+        { upsert: true }
+      );
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return {
     data: new SlashCommandBuilder()
       .setName("get-quotes")
@@ -70,7 +82,7 @@ module.exports = (database, client) => {
       try {
         const channel = client.channels.cache.get(process.env.channel_id);
         const messages = await channel.messages.fetch();
-        const data = processQuotes(client, messages);
+        const data = processQuotes(messages);
         await interaction.reply({
           content: `Read and processed ${data.count} Quotes in ${data.elapsedTime}ms.`,
           ephemeral: true,
