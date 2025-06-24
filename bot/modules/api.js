9caa5f3b4485e9def533cddb6088ac177ebf3254
Traube <girlscoutcookies420@gmx.net>
2025-06-24 20:46:58 +0200

    Feat: filter quotes & add user obejcts

--- bot/modules/api.js
+++ bot/modules/api.js
@@ -13,7 +13,69 @@ module.exports = (database) => {
       const respond = (content) => res.json(content);
       if (quotes) return respond(quotes);
 
-      quotes = await database.collection("quotes").find().toArray();
+      const pipeline = [
+        {
+          $lookup: {
+            from: "users",
+            localField: "authorIds",
+            foreignField: "_id",
+            as: "authors",
+          },
+        },
+        {
+          $addFields: {
+            authors: {
+              $map: {
+                input: "$authors",
+                as: "author",
+                in: {
+                  avatar: "$$author.avatar",
+                  name: "$$author.name",
+                  username: "$$author.username",
+                },
+              },
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "publisherId",
+            foreignField: "_id",
+            as: "publisherInfo",
+          },
+        },
+        {
+          $addFields: {
+            publisherInfo: {
+              $map: {
+                input: "$publisherInfo",
+                as: "pub",
+                in: {
+                  avatar: "$$pub.avatar",
+                  name: "$$pub.name",
+                  username: "$$pub.username",
+                },
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            serverId: 1,
+            createdTimestamp: 1,
+            content: 1,
+            authors: 1,
+            publisher: { $first: "$publisherInfo" },
+          },
+        },
+      ];
+
+      quotes = await database
+        .collection("quotes")
+        .aggregate(pipeline)
+        .toArray();
 
       cache.set("quotes", quotes);
 
@@ -25,4 +87,4 @@ module.exports = (database) => {
 
   const port = process.env.api_port || 8080;
   app.listen(port, () => console.log(`API running on port ${port}`));
-}
+};
