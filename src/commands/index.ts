e4cf607920f0a023ec2d8b0b2d989a889bcca3e5
Aseraphyss <71942852+Aseraphyss@users.noreply.github.com>
2026-03-08 23:06:01 +0100

    chore: auto update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,11 @@
 import * as ping from "./ping";
-import * as setChannel from "./setChannel";
 import * as quote from "./quote";
 import * as quoteMuch from "./quoteMuch";
+import * as setChannel from "./setChannel";
 
 export const commands = {
-    ping,
-    "set-channel": setChannel,
-    quote: quote,
-    "quote-much": quoteMuch,
+    [ping.data.name]: ping,
+    [quote.data.name]: quote,
+    [quoteMuch.data.name]: quoteMuch,
+    [setChannel.data.name]: setChannel,
 };
