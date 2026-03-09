3afae5cd13397a54f1595c49bfb2288576daec56
Aseraphyss <71942852+Aseraphyss@users.noreply.github.com>
2026-03-09 01:52:26 +0100

    chore: apply eslint rules correctly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,14 +6,15 @@ import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig(
-    globalIgnores(["dist/**", "node_modules/**"]),
-    {
-        rules: {
-            "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-        },
-    },
     eslint.configs.recommended,
     tseslint.configs.strict,
     tseslint.configs.stylistic,
-    eslintConfigPrettier
+    eslintConfigPrettier,
+    {
+        rules: {
+            "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+    globalIgnores(["dist/**", "node_modules/**"])
 );
