ea7c4ab2ba122c8add4d16323d4fb9968d3d2236
Aseraphyss <71942852+Aseraphyss@users.noreply.github.com>
2026-03-10 23:12:00 +0100

    feat: hard catch for bad errors

--- src/index.ts
+++ src/index.ts
@@ -13,32 +13,36 @@ client.on(Events.GuildCreate, async (guild) => {
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const { commandName } = interaction;
-    if (!(commandName in commands)) {
-        return interaction.reply({
-            content: "Unknown command!",
-            flags: MessageFlags.Ephemeral,
-        });
-    }
-    const command = commands[commandName as keyof typeof commands];
-
     try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-                content: "There was an error while executing this command!",
-                flags: MessageFlags.Ephemeral,
-            });
-        } else {
-            await interaction.reply({
-                content: "There was an error while executing this command!",
+        if (!interaction.isChatInputCommand()) return;
+
+        const { commandName } = interaction;
+        if (!(commandName in commands)) {
+            return interaction.reply({
+                content: "Unknown command!",
                 flags: MessageFlags.Ephemeral,
             });
         }
+        const command = commands[commandName as keyof typeof commands];
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: "There was an error while executing this command!",
+                    flags: MessageFlags.Ephemeral,
+                });
+            } else {
+                await interaction.reply({
+                    content: "There was an error while executing this command!",
+                    flags: MessageFlags.Ephemeral,
+                });
+            }
+        }
+    } catch (error) {
+        console.error("Could not reply to interaction:", error);
     }
 });
 
